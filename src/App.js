70f8b0b9b76835a8ddf07cfb80e32e8893f224f3
Nikki Christofi <nikki.christofi@gmail.com>
2018-11-11 16:47:37 +0000

    Add mapbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import logo from './logo.svg';
 import './App.css';
+
+const Map = ReactMapboxGl({
+  accessToken: 'pk.eyJ1IjoicnVuc3luYyIsImEiOiJjajdsdHY2N3cyNHgzMzJ0NTVkeDV5eHFnIn0.S6BvMFX212qFHTY8nPEOWQ'
+});
 
 class App extends Component {
 
@@ -14,10 +19,11 @@ class App extends Component {
 
   async componentWillMount() {
     const apiRoute = `${process.env.REACT_APP_API_PATH_DOMAIN}`
-    const { data: { holidays } } = await fetch(`${apiRoute}/data`, { method: 'GET' })
+    const { holidays: { data: holidays } } = await fetch(`${apiRoute}/data`, { method: 'GET' })
       .then(response => response.json())
       .catch(error => console.error('apiFetch error', error))
 
+    console.log('HOLIDAYS', holidays)
     this.setState({ holidays })
   }
 
@@ -25,17 +31,21 @@ class App extends Component {
     const { holidays } = this.state
 
     return (
-      <div className="App">
-       { holidays && (
-          holidays.map(hol => (
-            <div key={hol.name}>
-              <div>{hol.name}</div>
-              <div>{hol.location}</div>
-            </div>
-          ))
-        )
-       }
-      </div>
+      <Map
+        style="mapbox://styles/runsync/cjod3ea902rje2spac5kk29ld"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw"
+        }}
+        center={[-0.119634, 34.979101]}
+        zoom={[1.63]}>
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ "icon-image": "marker-15" }}>
+            <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+          </Layer>
+      </Map>
     );
   }
 }
