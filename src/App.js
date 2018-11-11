07c9ecaa8d04db6c9adc4071df23697a7f2fd9c5
Nikki Christofi <nikki.christofi@gmail.com>
2018-11-11 15:39:51 +0000

    Hook up frontend to api and show data in simple way initially

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      holidays: null
+    }
+  }
+
+  async componentWillMount() {
+    const { data: { holidays } } = await fetch('http://localhost:8081/data', { method: 'GET' })
+      .then(response => response.json())
+      .catch(error => console.error('apiFetch error', error))
+
+    this.setState({ holidays })
+  }
+
+  render() {
+    const { holidays } = this.state
+
+    return (
+      <div className="App">
+       { holidays && (
+          holidays.map(hol => (
+            <div key={hol.name}>
+              <div>{hol.name}</div>
+              <div>{hol.location}</div>
+            </div>
+          ))
+        )
+       }
+      </div>
+    );
+  }
+}
+
+export default App;
